3607ffaec22070eb484d02ea4e98a76a8c899626
inamahmadbaig <inamahmadbaig@gmail.com>
2026-06-29 14:50:35 +0530

    fix: Disable Hibernate DDL auto to prevent SchemaMigrator from opening concurrent connections and hitting DB limit

--- TestDB.java
+++ TestDB.java
@@ -1,6 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestDB {
     public static void main(String[] args) {
@@ -11,6 +13,16 @@ public class TestDB {
         System.out.println("Testing connection to: " + url);
         try (Connection conn = DriverManager.getConnection(url, user, pass)) {
             System.out.println("Connection successful!");
+            try (Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery("SHOW PROCESSLIST")) {
+                System.out.println("--- ACTIVE CONNECTIONS ---");
+                while (rs.next()) {
+                    System.out.println(String.format("ID: %s, User: %s, Host: %s, DB: %s, Command: %s, Time: %s, State: %s, Info: %s",
+                            rs.getString("Id"), rs.getString("User"), rs.getString("Host"),
+                            rs.getString("db"), rs.getString("Command"), rs.getString("Time"),
+                            rs.getString("State"), rs.getString("Info")));
+                }
+            }
         } catch (SQLException e) {
             System.out.println("Connection failed!");
             e.printStackTrace();
